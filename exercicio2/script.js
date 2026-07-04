15b5189f2f450182ca0abd93aaa1b6ef43465396
elcioveiga <elcio.veiga@escola.pr.gov.br>
2026-07-04 14:28:33 -0300

    Correção do codigo

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+
+ function modoClaro() {
+    document.body.style.backgroundColor = "lightcoral";
+    document.body.style.color = "darkred";
+    
+    document.getElementById("mensagem").innerText ="Modo claro ativado!";
+}
+
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "darkred";
+    document.body.style.color = "lightcoral";
+    
+    document.getElementById("mensagem").innerText ="Modo escuro ativado!";
+}
